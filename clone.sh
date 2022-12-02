4980d064325ef6035acff8eca080eebe3b50dd6e
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-02 21:48:41 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,6 +2,7 @@
 # -(c) zsecc0de-crew.id ~ widhisec
 # -createon   : 24/04/2019
 # -toolsname  : clone github
+# -lastupdate : 02/12/2022
 GREEN=$(tput setaf 2)
 RED=$(tput setaf 1)
 NORMAL=$(tput sgr0)
@@ -21,15 +22,14 @@ checking
 user(){
 local g ba
 g=$(curl -s https://api.github.com/users/$w/repos)
-ba=$(echo $g | grep -Po '(?<="clone_url":)[^,]*' |  
-gawk '{print $1}' | tr -d '""' | 
-gawk -F //git. '{ print $2 }' | 
-cut -d "." -f2 | gawk -F / '{ print $3 }'|sort -u)
+ba=$(echo $g | grep -Po '(?<="clone_url":)[^,]*'|tr -d '"'|gawk -F //git. '{ print $2 }'|
+gawk -F / '{ print $3 }'|
+cut -d '.' -f1)
 for arrItem in "${ba[@]}"
 do
   select nu in $arrItem
 do
-        echo "$nu"
+        echo $nu
         break
    done
 done
@@ -53,7 +53,7 @@ Powered by zsecc0de-crew.id
 "
 echo -en "${RED}[?] ${NORMAL}name > "; read w
 user $w
-# echo -en "${GREEN}[*] ${NORMAL}pilih > "; read pilih
+echo -en "${GREEN}[*] ${NORMAL}pilih > "; read pilih
 main $j;
 git clone `cat down`
 rm -rf user
